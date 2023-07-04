115d2599a7a1c9ad80f9ce6796d679d209d795c6
Tine Margretha Vister <tine.margretha.vister@dnb.no>
2023-07-04 14:27:07 +0200

    GH-28: laget et utkast for betal modal

--- src/code/Customer/Account/PayOrTransferMoney.tsx
+++ src/code/Customer/Account/PayOrTransferMoney.tsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { Button, Dialog } from "@dnb/eufemia";
+import {
+  Button,
+  DatePicker,
+  Dialog,
+  Dropdown,
+  FormRow,
+  FormSet,
+  InputMasked,
+  Textarea,
+} from "@dnb/eufemia";
 import pay_from from "@dnb/eufemia/icons/dnb/pay_from";
 import transfer_to from "@dnb/eufemia/icons/dnb/transfer_to";
 
@@ -12,36 +21,71 @@ export default function PayOrTransferMoney() {
     <>
       <Dialog
         variant="confirmation"
-        title="Betal"
-        trigger={(props) => (
-          <Button
-            {...props}
-            icon={transfer_to}
-            icon_position="left"
-            size="large"
-            text="Betal"
-            variant="primary"
-          />
-        )}
-      >
-        Si hvem du vil betale penger til.
-      </Dialog>
-      <Dialog
-        title="Overfør"
+        title="Betale til"
+        icon={pay_from}
+        onConfirm={({ close }) => close()}
+        confirmText="Betal penger"
+        triggerAttributes={{ text: "Betal" }}
         trigger={(props) => (
           <Button
             {...props}
             icon={pay_from}
             icon_position="left"
             size="large"
-            text="Overfør"
+            variant="primary"
+          />
+        )}
+      >
+        <FormSet space="xx-large" responsive>
+          <Dropdown space label="Betale til:" />
+          <Dropdown space label="Betale fra:" />
+          <DatePicker
+            space
+            label="Dato:"
+            date={new Date()}
+            show_input
+          ></DatePicker>
+          <Textarea
+            space
+            label="Beskrivelse:"
+            rows={1}
+            autoresize
+            autoresize_max_rows={1}
+            on_key_down={({ rows, event }) => {
+              if (rows >= 1 && event.key === "Enter") {
+                event.preventDefault();
+              }
+            }}
+          />
+          <InputMasked
+            space
+            label="Beløp:"
+            as_currency="NOK"
+            on_change={({ numberValue }) => {
+              console.log(numberValue);
+            }}
+          />
+        </FormSet>
+      </Dialog>
+
+      <Dialog
+        variant="confirmation"
+        title="Overfør tittel"
+        icon={transfer_to}
+        onConfirm={({ close }) => close()}
+        confirmText="Overfør penger"
+        triggerAttributes={{ text: "Overfør" }}
+        trigger={(props) => (
+          <Button
+            {...props}
+            icon={transfer_to}
+            icon_position="left"
+            size="large"
             variant="secondary"
             left
           />
         )}
-      >
-        Si hvem du vil overføre penger til.
-      </Dialog>
+      />
     </>
   );
 }
