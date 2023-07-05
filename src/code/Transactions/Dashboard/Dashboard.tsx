bb90e61e6ad63ff2d75742b0fa52e3f277fb9c0f
Marie Wilhelmsen <marie.wilhelmsen@dnb.no>
2023-07-05 12:57:42 +0200

    Moved PieChart and BarChart to ignore. Made a dictionary of colors in Colors.tsx in ignore

--- src/code/Transactions/Dashboard/Dashboard.tsx
+++ src/code/Transactions/Dashboard/Dashboard.tsx
@@ -1,29 +1,31 @@
 import React from "react";
-import PieChart from "./PieChart";
-import { H1, H2, InfoCard } from "@dnb/eufemia";
-import BarChart from "./BarChart";
+import PieChart from "../../../ignore/PieChart";
+import { H1, InfoCard } from "@dnb/eufemia";
+import BarChart from "../../../ignore/BarChart";
 import "../../../ignore/styles.css";
-import { card_in_medium, hand_money_medium, profile_medium } from "@dnb/eufemia/icons";
-
+import { account_medium, card_in_medium, people_1_medium } from "@dnb/eufemia/icons";
+import colorPicker from "../../../ignore/Colors";
 
 const barChartData = {
-    labels: ['21', '22', '23', '24', '25', '26'],
+    labels: ['21', '22', '23', '24', '25', '26'], //TODO: Passende labels
     datasets: [
         {
         label: 'Sales',
-        data: [100, 200, 150, 300, 250, 180],
-        backgroundColor: '#5DADE2', 
+        data: [100, 200, 150, 300, 250, 180],  //TODO: Hent data fra database / datasett
+        backgroundColor: colorPicker.SuccessGreen, 
         },
     ],    
 }
 
 const pieChartData = {
-    labels: ['Red', 'Blue', 'Yellow'],
+    labels: ['Red', 'Blue', 'Yellow'], //TODO: Passende labels
     datasets: [
         {
-        data: [300, 50, 100, 75],
-        backgroundColor: ['#5DADE2', "#27AE60", '#7DCEA0', '#C39BD3'],
-        hoverBackgroundColor: ['#5DADE2',"#27AE60", '#7DCEA0','#C39BD3',],
+        data: [300, 200, 150, 75, 50, 10], //TODO: Hent data fra database / datasett
+        
+        backgroundColor: [colorPicker.Indigo, colorPicker.Violet, colorPicker.SeaGreen, colorPicker.SuccessGreen, colorPicker.SummerGreen, colorPicker.MintGreen],
+
+        hoverColor: [colorPicker.Indigo, colorPicker.Violet, colorPicker.SeaGreen, colorPicker.SuccessGreen, colorPicker.SummerGreen, colorPicker.MintGreen]
         },
     ],    
 }
@@ -46,24 +48,23 @@ export default function Dashboard() {
             <div className="DashboardBottom">
                 <InfoCard 
                     title="Antall kunder"
-                    text="4"
-                    icon={profile_medium}
+                    text="4" // TODO: Hent fra database / datasett
+                    icon={people_1_medium}
                     space="x-small"                    
                 />
                 <InfoCard 
                     title="Antall transaksjoner"
-                    text="100"
+                    text="100" // TODO: Hent fra database / datasett
                     icon={card_in_medium}
                     space="x-small"
                 />
                 <InfoCard 
                     title="Penger i omløp"
-                    text="19 milliarder"
-                    icon={hand_money_medium}
+                    text="19 milliarder" // TODO: Hent fra database / datasett
+                    icon={account_medium}
                     space="x-small"
                 />
             </div>
-            
         </div>
     )
 }
