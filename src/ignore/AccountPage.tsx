6da823b249d3f6f36a1e7b847af6849b13465c7e
Regine Ruud <regine.ruud@dnb.no>
2023-10-23 09:08:27 +0200

    database task

--- src/ignore/AccountPage.tsx
+++ src/ignore/AccountPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Breadcrumb, H1, H2, H3, NumberFormat, Section } from "@dnb/eufemia";
-import { Account, Transaction } from "./Models";
+import { Account, Transaction } from "../data/Models";
 import { transfer_to, pay_from } from "@dnb/eufemia/icons";
 import { TransactionTableForAccounts } from "./TransactionTableForAccounts";
 
