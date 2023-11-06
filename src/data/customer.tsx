718d3406cedcef06b2f17a652a6df7f2a10758d3
Marie Wilhelmsen <marie.wilhelmsen@dnb.no>
2023-11-06 15:36:50 +0100

    Changed the ids in customer.tsx

--- src/data/customer.tsx
+++ src/data/customer.tsx
@@ -7,223 +7,229 @@ export var customer = {
             number: "1592.33.12123",
             transactions: [
                 {
-                    id: "1",
+                    id: "100",
                     date: "14.11.23",
                     description: "Vipps: Sodexo Dnb Bj",
                     amount: "-48"
                 },
                 {
-                    id: "2",
+                    id: "101",
+                    date: "14.11.23",
+                    description: "Børsbaren",
+                    amount: "-45"
+                },
+                {
+                    id: "102",
                     date: "13.11.23",
                     description: "Rema 1000 Billingstad",
                     amount: "-161.60"
                 },
                 {
-                    id: "3",
+                    id: "103",
                     date: "13.11.23",
                     description: "Norsk Tipping Abonnement",
                     amount: "-50.00"
                 },
                 {
-                    id: "4",
+                    id: "104",
                     date: "13.11.23",
                     description: "Vipps: Sodexo Dnb Bj",
                     amount: "-66"
                 },
                 {
-                    id: "5",
+                    id: "105",
                     date: "13.11.23",
                     description: "Sodexo: Børsbaren",
                     amount: "-42"
                 },
                 {
-                    id: "6",
+                    id: "106",
                     date: "12.11.23",
                     description: "Oda.no",
                     amount: "-2750.25"
                 },
                 {
-                    id: "7",
+                    id: "107",
                     date: "11.11.23",
                     description: "BA3",
                     amount: "-362"
                 },
                 {
-                    id: "8",
+                    id: "108",
                     date: "10.11.23",
                     description: "Paycheck",
                     amount: "34000"
                 },
                 {
-                    id: "9",
+                    id: "109",
                     date: "09.11.23",
                     description: "Vipps: Sodexo Dnb Bj",
                     amount: "-45"
                 },
                 {
-                    id: "10",
+                    id: "110",
                     date: "08.11.23",
                     description: "Granny",
                     amount: "500"
                 },
                 {
-                    id: "11",
+                    id: "111",
                     date: "07.11.23",
                     description: "Kiwi Bjørvika",
                     amount: "-25"
                 },
                 {
-                    id: "12",
+                    id: "112",
                     date: "06.11.23",
                     description: "Kiwi Bjørvika",
                     amount: "-15"
                 },
                 {
-                    id: "13",
+                    id: "113",
                     date: "01.11.23",
                     description: "Vipps: Sodexo Dnb Bj",
                     amount: "-38"
                 },
                 {
-                    id: "14",
+                    id: "114",
                     date: "01.11.23",
                     description: "Savings Account",
                     amount: "-2500"
                 },
                 {
-                    id: "15",
+                    id: "115",
                     date: "31.10.23",
                     description: "Rent",
                     amount: "-15700"
                 },
                 {
-                    id: "16",
+                    id: "116",
                     date: "31.10.23",
                     description: "Vipps: Sodexo Dnb Bj",
                     amount: "-48"
                 },
                 {
-                    id: "17",
+                    id: "117",
                     date: "21.10.23",
                     description: "Vinmonopolet",
                     amount: "-310"
                 },
                 {
-                    id: "18",
+                    id: "118",
                     date: "21.10.23",
                     description: "Åpent Bakeri",
                     amount: "-139"
                 },
                 {
-                    id: "19",
+                    id: "119",
                     date: "18.10.23",
                     description: "Rema 1000 Billingstad",
                     amount: "-320"
                 },
                 {
-                    id: "20",
+                    id: "120",
                     date: "17.10.23",
                     description: "Burger King",
                     amount: "-164"
                 },
                 {
-                    id: "21",
+                    id: "121",
                     date: "16.10.23",
                     description: "Norsk Tipping Abonnement",
                     amount: "-50"
                 },
                 {
-                    id: "22",
+                    id: "122",
                     date: "16.10.23",
                     description: "Tibber",
                     amount: "-587"
                 },
                 {
-                    id: "23",
+                    id: "123",
                     date: "16.10.23",
                     description: "SATS",
                     amount: "-591.20"
                 },
                 {
-                    id: "24",
+                    id: "124",
                     date: "16.10.23",
                     description: "Netflix",
                     amount: "-159"
                 },
                 {
-                    id: "25",
+                    id: "125",
                     date: "16.10.23",
                     description: "HBO: Max",
                     amount: "-49.50"
                 },
                 {
-                    id: "26",
+                    id: "126",
                     date: "16.10.23",
                     description: "TV Play",
                     amount: "-209"
                 },
                 {
-                    id: "27",
+                    id: "127",
                     date: "16.10.23",
                     description: "Car loan",
                     amount: "-4920"
                 },
                 {
-                    id: "28",
+                    id: "128",
                     date: "16.10.23",
                     description: "Ruter 30-day Ticket",
                     amount: "-853"
                 },
                 {
-                    id: "29",
+                    id: "129",
                     date: "14.10.23",
                     description: "Louise Bar & Restaurant",
                     amount: "-970"
                 },
                 {
-                    id: "30",
+                    id: "130",
                     date: "14.10.23",
                     description: "Ark Bookstore",
                     amount: "-375"
                 },
                 {
-                    id: "31",
+                    id: "131",
                     date: "14.10.23",
                     description: "Grændsens Skotøumagazin AS",
                     amount: "-2200"
                 },
                 {
-                    id: "32",
+                    id: "132",
                     date: "14.10.23",
                     description: "COS",
                     amount: "-4753"
                 },
                 {
-                    id: "33",
+                    id: "133",
                     date: "11.10.23",
                     description: "Zalando",
                     amount: "-2075.50"
                 },
                 {
-                    id: "34",
+                    id: "134",
                     date: "10.10.23",
                     description: "Børsbaren",
                     amount: "-58"
                 },
                 {
-                    id: "35",
+                    id: "135",
                     date: "10.10.23",
                     description: "Vipps: Sodexo Dnb Bj",
                     amount: "-70"
                 },
                 {
-                    id: "36",
+                    id: "136",
                     date: "09.10.23",
                     description: "Paycheck",
                     amount: "34000"
                 },
                 {
-                    id: "37",
+                    id: "137",
                     date: "05.10.23",
                     description: "Mum",
                     amount: "275"
@@ -236,85 +242,85 @@ export var customer = {
             number: "9411.05.96614",
             transactions: [
                 {
-                    id: "38",
+                    id: "200",
                     date: "01.11.23",
                     description: "Checking Account",
                     amount: "2500"
                 },
                 {
-                    id: "39",
+                    id: "201",
                     date: "02.10.23",
                     description: "Checking Account",
                     amount: "2500"
                 },
                 {
-                    id: "40",
+                    id: "202",
                     date: "01.09.23",
                     description: "Checking Account",
                     amount: "2500"
                 },
                 {
-                    id: "41",
+                    id: "203",
                     date: "01.09.23",
                     description: "Mum",
                     amount: "1500"
                 }, 
                 {
-                    id: "42",
+                    id: "204",
                     date: "01.09.23",
                     description: "Dad",
                     amount: "1500"
                 },
                 {
-                    id: "43",
+                    id: "205",
                     date: "01.09.23",
                     description: "Granny",
                     amount: "2700"
                 },
                 {
-                    id: "44",
+                    id: "206",
                     date: "01.09.23",
                     description: "Granpa",
                     amount: "2500"
                 },
                 {
-                    id: "45",
+                    id: "207",
                     date: "01.09.23",
                     description: "Brother",
                     amount: "500"
                 },
                 {
-                    id: "46",
+                    id: "208",
                     date: "01.08.23",
                     description: "Checking Account",
                     amount: "5500"
                 },
                 {
-                    id: "47",
+                    id: "209",
                     date: "03.07.23",
                     description: "Checking Account",
                     amount: "10500"
                 },
                 {
-                    id: "48",
+                    id: "210",
                     date: "01.06.23",
                     description: "Checking Account",
                     amount: "2500"
                 },
                 {
-                    id: "49",
+                    id: "211",
                     date: "02.05.23",
                     description: "Checking Account",
                     amount: "2500"
                 },
                 {
-                    id: "50",
+                    id: "212",
                     date: "03.07.23",
                     description: "Checking Account",
                     amount: "2500"
                 },
                 {
-                    id: "51",
+                    id: "213",
                     date: "03.04.23",
                     description: "Checking Account",
                     amount: "2500"
@@ -327,19 +333,19 @@ export var customer = {
             number: "2344.90.41249",
             transactions: [
                 {
-                    id: "52",
+                    id: "300",
                     date: "02.03.23",
                     description: "Checking Account",
                     amount: "10000"
                 },
                 {
-                    id: "53",
+                    id: "301",
                     date: "03.02.23",
                     description: "Checking Account",
                     amount: "7500"
                 },
                 {
-                    id: "54",
+                    id: "302",
                     date: "03.01.23",
                     description: "Checking Account",
                     amount: "10000"
